68f2ba44f9815d919d75c3ade4271b9b31d62248
Hamdi Adan <hhhaji013@gmail.com>
2024-05-13 22:38:51 +0300

    Added restful routes and models for category and product

--- app/app.py
+++ app/app.py
@@ -27,6 +27,166 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Restful Routes
 
+# Routes for Products
+class Products(Resource):
+    def get(self):
+        products = [product.to_dict() for product in Product.query.all()]
+        return make_response(products, 200)
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return {"error": "Missing data in request"}, 400
+
+        title = data.get('title')
+        description = data.get('description')
+        seller_id = data.get('seller_id')
+        price = data.get('price')
+        quantity = data.get('quantity')
+        category_id = data.get('category_id')
+
+        # Validate input data
+        if not all([title, description, seller_id, price, quantity, category_id]):
+            return {"error": "Missing required fields"}, 400
+
+        seller = User.query.get(seller_id)
+        category = Category.query.get(category_id)
+
+        if not seller:
+            return {"error": "Seller not found"}, 404
+
+        if not category:
+            return {"error": "Category not found"}, 404
+
+        product = Product(
+            title=title,
+            description=description,
+            seller_id=seller_id,
+            price=price,
+            quantity=quantity,
+            category_id=category_id
+        )
+
+        db.session.add(product)
+        db.session.commit()
+        return make_response(product.to_dict(), 201)
+
+
+api.add_resource(Products, '/products')
+
+class ProductsByID(Resource):
+    def get(self, id):
+        product = Product.query.get(id)
+        if product:
+            return make_response(product.to_dict(), 200)
+        else:
+            return {"error": "Product not found"}, 404
+
+    def patch(self, id):
+        product = Product.query.get(id)
+        if not product:
+            return {"error": "Product not found"}, 404
+
+        data = request.get_json()
+
+        # Validate input data
+        if not data:
+            return {"error": "Missing data in request"}, 400
+
+        valid_fields = ['title', 'description', 'seller_id', 'price', 'quantity', 'category_id']
+        if not all(field in data for field in valid_fields):
+            return {"error": "Missing or invalid fields in request"}, 400
+
+        seller = User.query.get(data.get('seller_id'))
+        category = Category.query.get(data.get('category_id'))
+
+        if not seller:
+            return {"error": "Seller not found"}, 404
+
+        if not category:
+            return {"error": "Category not found"}, 404
+
+        product.title = data.get('title')
+        product.description = data.get('description')
+        product.seller_id = data.get('seller_id')
+        product.price = data.get('price')
+        product.quantity = data.get('quantity')
+        product.category_id = data.get('category_id')
+
+        db.session.commit()
+        return make_response(product.to_dict(), 200)
+
+    def delete(self, id):
+        product = Product.query.get(id)
+        if not product:
+            return {"error": "Product not found"}, 404
+
+        db.session.delete(product)
+        db.session.commit()
+        return {"message": "Product deleted successfully"}, 200
+
+
+api.add_resource(ProductsByID, '/products/<int:id>')
+
+#Route for Categories
+class Categories(Resource):
+    def get(self):
+        categories = [category.to_dict() for category in Category.query.all()]
+        return make_response(categories, 200)
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return {"error": "Missing data in request"}, 400
+
+        category_name = data.get('category_name')
+        if not category_name:
+            return {"error": "Missing category name"}, 400
+
+        category = Category(category_name=category_name)
+
+        db.session.add(category)
+        db.session.commit()
+        return make_response(category.to_dict(), 201)
+
+
+api.add_resource(Categories, '/categories')
+
+class CategoriesByID(Resource):
+    def get(self, id):
+        category = Category.query.get(id)
+        if category:
+            return make_response(category.to_dict(), 200)
+        else:
+            return {"error": "Category not found"}, 404
+
+    def patch(self, id):
+        category = Category.query.get(id)
+        if not category:
+            return {"error": "Category not found"}, 404
+
+        data = request.get_json()
+        category_name = data.get('category_name')
+        if not category_name:
+            return {"error": "Missing category name"}, 400
+
+        category.category_name = category_name
+        db.session.commit()
+        return make_response(category.to_dict(), 200)
+
+    def delete(self, id):
+        category = Category.query.get(id)
+        if not category:
+            return {"error": "Category not found"}, 404
+
+        db.session.delete(category)
+        db.session.commit()
+        return {"message": "Category deleted successfully"}, 200
+
+
+api.add_resource(CategoriesByID, '/categories/<int:id>')
+
+
 
 
 if __name__ == '__main__':
