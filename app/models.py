23b4a717aba8356797e3c09211dd70070ddb915a
Hamdi <117962769+HamdiAdan@users.noreply.github.com>
2024-05-14 10:32:22 +0300

    Added restful routes and models for category and product (#6)
    * Added restful routes and models for category and product
    
    * Added serialization rules
    
    ---------
    
    Co-authored-by: Alex Kimeu <alexkimeu1999@gmail.com>

--- app/models.py
+++ app/models.py
@@ -133,11 +133,34 @@ class Wishlist(db.Model, SerializerMixin):
 # Product Model
 class Product(db.Model, SerializerMixin):
     __tablename__= 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.Text)
+    seller_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    price = db.Column(db.String)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    quantity = db.Column(db.Integer)
+    images = db.Column(db.Text)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    reviews = db.relationship('Review', back_populates='product', lazy=True)
+    cart_items = db.relationship('Cart', back_populates='product', lazy=True)
+    wishlist_items = db.relationship('Wishlist', back_populates='product', lazy=True)
+    store = db.relationship('Store', back_populates='products')
+    category = db.relationship('Category', back_populates='products')
+
+    #serialize
+    serialize_rules = ( ' -reviews.product', '-cart-items.product', '-wishlist_items.product','-store.products','category.products')
+
     
-    pass
 
 # Category Model
 class Category(db.Model, SerializerMixin):
     __tablename__= 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String)
+    products = db.relationship('Product', back_populates='category', lazy=True)
+
+    #serialize 
+    serialize_rules= ('-products.category')
+
     
-    pass
